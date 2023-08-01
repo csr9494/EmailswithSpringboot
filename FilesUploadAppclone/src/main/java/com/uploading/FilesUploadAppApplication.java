d47602b7040059ff1e41a3a895ad0a47980d8532
csr9494 <120386409+csr9494@users.noreply.github.com>
2023-08-01 14:39:25 +0530

    changed code

--- FilesUploadAppclone/src/main/java/com/uploading/FilesUploadAppApplication.java
+++ FilesUploadAppclone/src/main/java/com/uploading/FilesUploadAppApplication.java
@@ -8,7 +8,7 @@ public class FilesUploadAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilesUploadAppApplication.class, args);
-		System.out.println("heloo");
+		System.out.println("i am file transfermission");
 	}
 
 }
